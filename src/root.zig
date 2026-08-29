0be3722094dc1cb8775bd071a122a47ccffff8e5
Comrade Lemoncake <jdbohrman@outlook.com>
2026-08-29 06:52:40 -0400

    Refactor: keep src root focused on module wiring

--- src/root.zig
+++ src/root.zig
@@ -34,70 +34,8 @@ test {
     _ = protocol_model_summary;
 }
 
-test "messages route deterministically and update operator state" {
-    const TestRuntime = runtime.Runtime(8, 32, 32);
-    var rt = TestRuntime.init(42);
+pub const root_tests = @import("tests/root_runtime.zig");
 
-    try rt.addOperator(.{ .id = 1, .transition = operator.echo });
-    try rt.addOperator(.{ .id = 2, .transition = operator.accumulator });
-
-    var causal = content_id.zero;
-    causal[0] = 99;
-
-    try rt.enqueue(.{
-        .sender = 2,
-        .recipient = 1,
-        .kind = .query,
-        .payload = 7,
-        .causal_ref = causal,
-    });
-    try rt.run();
-
-    try std.testing.expectEqual(@as(u64, 1), rt.operators[0].state);
-    try std.testing.expectEqual(@as(u64, 7), rt.operators[1].state);
-    try std.testing.expectEqual(@as(usize, 2), rt.trace_len);
-    try std.testing.expectEqual(@as(u64, 1), rt.trace[0].sequence);
-    try std.testing.expectEqual(@as(u64, 2), rt.trace[1].sequence);
-    try std.testing.expectEqual(message.Kind.evidence, rt.trace[1].message.kind);
-    try std.testing.expect(rt.trace[1].message.causal_ref != null);
-    try std.testing.expect(content_id.eql(causal, rt.trace[1].message.causal_ref.?));
-}
-
-test "same seed produces the same experimental entropy" {
-    const TestRuntime = runtime.Runtime(1, 1, 1);
-    var a = TestRuntime.init(0x5eed);
-    var b = TestRuntime.init(0x5eed);
-
-    var i: usize = 0;
-    while (i < 32) : (i += 1) {
-        try std.testing.expectEqual(a.randomIndex(17), b.randomIndex(17));
-    }
-}
-
-test "different seeds diverge" {
-    const TestRuntime = runtime.Runtime(1, 1, 1);
-    var a = TestRuntime.init(1);
-    var b = TestRuntime.init(2);
-
-    var different = false;
-    var i: usize = 0;
-    while (i < 16) : (i += 1) {
-        if (a.randomIndex(1024) != b.randomIndex(1024)) different = true;
-    }
-    try std.testing.expect(different);
-}
-
-test "duplicate operators are rejected" {
-    const TestRuntime = runtime.Runtime(2, 1, 1);
-    var rt = TestRuntime.init(0);
-    const op: operator.Operator = .{ .id = 7, .transition = operator.accumulator };
-    try rt.addOperator(op);
-    try std.testing.expectError(error.DuplicateOperator, rt.addOperator(op));
-}
-
-test "unknown recipients fail explicitly" {
-    const TestRuntime = runtime.Runtime(1, 2, 2);
-    var rt = TestRuntime.init(0);
-    try rt.enqueue(.{ .sender = 1, .recipient = 999, .kind = .claim });
-    try std.testing.expectError(error.UnknownRecipient, rt.step());
+test {
+    _ = root_tests;
 }
