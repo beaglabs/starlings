d328b6a13ea9732f867146f6ef3e0d0979525558
Comrade Lemoncake <jdbohrman@outlook.com>
2026-08-30 17:23:30 -0400

    Add runnable Phase 3 external pack fixture

---
+++ examples/packs/phase3-run/operators/derive.py
@@ -0,0 +1,15 @@
+import sys
+
+lines = [line for line in sys.stdin.read().splitlines() if line]
+operator = int(next(line.split("=", 1)[1] for line in lines if line.startswith("operator=")))
+provided = int(next(line.split("=", 1)[1] for line in lines if line.startswith("provide_var=")))
+record = next(line[4:] for line in lines if line.startswith("var="))
+_, _, encoded = record.split(",", 2)
+value = int(encoded.split(":", 1)[1])
+
+sys.stdout.write(
+    "STARLINGS/1 RESPONSE\n"
+    f"operator={operator}\n"
+    f"claim={provided},3,1000,{operator},i:{value + 1}\n"
+    "END\n"
+)
