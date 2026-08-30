8726b4fec5f4cdc4ab6f4d6c3cf239357d000ab8
Comrade Lemoncake <jdbohrman@outlook.com>
2026-08-30 19:05:27 -0400

    Add 3D emergence trial build step

--- build.zig
+++ build.zig
@@ -29,4 +29,20 @@ pub fn build(b: *std.Build) void {
     test_step.dependOn(&run_mod_tests.step);
     test_step.dependOn(&run_pack_tests.step);
 
+    const trial_3d_module = b.createModule(.{
+        .root_source_file = b.path("trials/3d-emergent-asset/trial.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    trial_3d_module.addImport("starlings", mod);
+    const trial_3d_tests = b.addTest(.{
+        .root_module = trial_3d_module,
+    });
+    const run_trial_3d_tests = b.addRunArtifact(trial_3d_tests);
+
+    const trial_3d_step = b.step(
+        "trial-3d",
+        "Run the emergent 3D asset population trial",
+    );
+    trial_3d_step.dependOn(&run_trial_3d_tests.step);
 }
