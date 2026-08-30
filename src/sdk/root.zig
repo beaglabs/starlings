7ea837365cca58c73c22fb36a0f58dff98041600
Comrade Lemoncake <jdbohrman@outlook.com>
2026-08-30 03:20:48 -0400

    Export SDK conformance suite

--- src/sdk/root.zig
+++ src/sdk/root.zig
@@ -3,7 +3,7 @@ pub const registry = @import("registry.zig");
 pub const eligibility = @import("eligibility.zig");
 pub const output_state = @import("output_state.zig");
 pub const execution = @import("execution.zig");
-pub const external = @import("external.zig");
+pub const external = @import("external.zig");\npub const conformance = @import("conformance.zig");
 
 test {
     _ = core;
@@ -11,5 +11,5 @@ test {
     _ = eligibility;
     _ = output_state;
     _ = execution;
-    _ = external;
+    _ = external;\n    _ = conformance;
 }
